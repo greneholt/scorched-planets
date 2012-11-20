961d4c4e2ca860ddd564abc26bf96af743b31822
Kevyn Young <keyoung@mines.edu>
2012-11-20 09:40:31 -0700

    Finished planetPlacingTest

--- src/tests/GameSetupTests.java
+++ src/tests/GameSetupTests.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import game.GameController;
 import game.Lander;
+import game.MapGenerator;
 import game.PhysicsSolver;
 import game.Planet;
 
@@ -16,28 +17,29 @@ public class GameSetupTests {
 
 	@Test
 	public void planetPlacingTest() {
-		PhysicsSolver testSolver;
-		GameController game = new GameController();
-		game.generatePlanets(5);
-		List<Planet> planets = new LinkedList<Planet>();
-		// TODO: initialize the following (probably in for loop) --> get access to planets generated
-		Planet p1 = null;
-		Planet p2 = null;
+		MapGenerator game = new MapGenerator();
+		List<Planet> planets = game.generatePlanets(5);
 		
-		assertTrue(p1.getPosition().subtract(p2.getPosition()).magnitude()>= p1.getRadius() 
-				+ p2.getRadius() + game.MIN_DISTANCE_BETWEEN_PLANETS);
+		for(Planet p1 : planets) {
+			for(Planet p2 : planets) {
+				if(!p1.equals(p2)) {
+					assertTrue(p1.getPosition().subtract(p2.getPosition()).magnitude()>= p1.getRadius() 
+							+ p2.getRadius() + game.MIN_DISTANCE_BETWEEN_PLANETS);
+				}
+			}
+		}
 	}
 	
 	@Test
 	public void landerPlacementTest() {
-		GameController game = new GameController();
+		/*GameController game = new GameController();
 		game.generatePlanets(4);
 		game.generateLanders();
 		// TODO: initialize the following --> get access to landers generated
 		Lander l1 = null;
 		Lander l2 = null;
 		Planet p1 = l1.getCurrentPlanet();
-		Planet p2 = l2.getCurrentPlanet();
+		Planet p2 = l2.getCurrentPlanet();*/
 		
 		// check to make sure there are no more than two landers on each planet
 		// check planets with 2 landers to make sure that two landers aren't in same hemisphere
