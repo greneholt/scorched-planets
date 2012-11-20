34be64ca685a7ec6c9d3a1e0081340f097eaf5cd
Connor McKay <connor@homeschooldebate.net>
2012-11-20 09:32:01 -0700

    Adding mapgenerator class

---
+++ src/game/MapGenerator.java
@@ -0,0 +1,17 @@
+package game;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MapGenerator {
+	public static float MIN_DISTANCE_BETWEEN_PLANETS = 20;
+
+	public List<Planet> generatePlanets(int n) {
+		return new LinkedList<Planet>();
+	}
+
+	// generates the specified number of landers and places them on the planets
+	public List<Lander> generateLanders(List<Planet> planets, int n) {
+		return new LinkedList<Lander>();
+	}
+}
